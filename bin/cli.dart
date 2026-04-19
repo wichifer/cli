15d824af91341f6290f2098b32ae41dbc52f43e1
Wichifer <mailbox.fernandez@gmail.com>
2026-04-19 02:54:58 -0300

    Initialize CLI project with basic structure and configuration files

---
+++ bin/cli.dart
@@ -0,0 +1,8 @@
+const version = '0.0.1'; // Add this line
+void main(List<String> arguments) {
+  if (arguments.isEmpty) {
+    print('Hello, Dart!');
+  } else if (arguments.first == 'version') {
+    print('Dartpedia CLI version $version');
+  }
+}
