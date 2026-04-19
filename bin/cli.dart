6b40e82fd65437835ee6511c7616112b21c8e704
Wichifer <mailbox.fernandez@gmail.com>
2026-04-19 02:56:10 -0300

    dart practices

--- bin/cli.dart
+++ bin/cli.dart
@@ -1,7 +1,7 @@
 const version = '0.0.1'; // Add this line
 void main(List<String> arguments) {
   if (arguments.isEmpty) {
-    print('Hello, Dart!');
+    print('Hola, Dart!');
   } else if (arguments.first == 'version') {
     print('Dartpedia CLI version $version');
   }
