dc4ee5d9153a314002075cd3ecd66274e98bfaf3
Wichifer <mailbox.fernandez@gmail.com>
2026-04-19 05:24:35 -0300

    Adding a printUsage function

--- bin/cli.dart
+++ bin/cli.dart
@@ -1,8 +1,17 @@
 const version = '0.0.1'; // Add this line
 void main(List<String> arguments) {
-  if (arguments.isEmpty) {
-    print('Hola, Dart!');
+  if (arguments.isEmpty || arguments.first == 'help') {
+    printUsage(); // Change this from 'Hello, Dart!'
   } else if (arguments.first == 'version') {
     print('Dartpedia CLI version $version');
+  } else {
+    printUsage(); // Catch-all for any unrecognized command.
   }
+}
+
+void printUsage() {
+  // Add this new function
+  print(
+    "The following commands are valid: 'help', 'version', 'search <ARTICLE-TITLE>'",
+  );
 }
